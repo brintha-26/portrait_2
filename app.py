8875f1d77dc1e9a77e42b9e949d3e030f15219ed
Brintha.S <brinthanagajothi@gmail.com>
2026-04-12 14:55:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -158,4 +158,5 @@ def admin_logout():
     return redirect(url_for("home"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+import os
+app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
